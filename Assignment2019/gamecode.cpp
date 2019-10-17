10cf80031079f7f2f41558c1dfc898290c9f8be4
Jamesadey98 <w16000810@northumbria.ac.uk>
2019-10-17 16:42:16 +0100

    Added everything bck

--- Assignment2019/gamecode.cpp
+++ Assignment2019/gamecode.cpp
@@ -14,12 +14,12 @@
 
 Game::Game()
 {
-   // No-op
+	// No-op
 }
 
 Game::~Game()
 {
-   // No-op
+	// No-op
 }
 
 Game Game::instance;    // Singleton instance
@@ -31,25 +31,25 @@ ErrorType Game::Main()
 
 {
 	//Flip and clear the back buffer
-	MyDrawEngine* pTheDrawEngine= MyDrawEngine::GetInstance();
+	MyDrawEngine* pTheDrawEngine = MyDrawEngine::GetInstance();
 	pTheDrawEngine->Flip();
 	pTheDrawEngine->ClearBackBuffer();
 
-	ErrorType err=SUCCESS;
+	ErrorType err = SUCCESS;
 
-	switch(m_currentState)
+	switch (m_currentState)
 	{
 	case MENU:
 
-		err= MainMenu();     // Menu at start of game
+		err = MainMenu();     // Menu at start of game
 		break;
 	case PAUSED:
 		err = PauseMenu();   // Player has paused the game
 		break;
 	case RUNNING:           // Playing the actual game
-		err= Update();
+		err = Update();
 		break;
-   case GAMEOVER:
+	case GAMEOVER:
 		err = FAILURE;       // Error return causes the window loop to exit
 	default:
 		// Not a valid state
@@ -64,16 +64,16 @@ void Game::ChangeState(GameState newState)
 {
 	// Very crude state system
 	// Close old state
-	switch(m_currentState)
+	switch (m_currentState)
 	{
 	case MENU:
-      // Not needed
+		// Not needed
 		break;
 	case PAUSED:
-      // Not needed
+		// Not needed
 		break;
 	case RUNNING:
-      // Not needed
+		// Not needed
 		break;
 	}
 
@@ -82,16 +82,16 @@ void Game::ChangeState(GameState newState)
 	m_menuOption = 0;
 
 	// Transition to new state
-	switch(m_currentState)
+	switch (m_currentState)
 	{
 	case MENU:
-      // Not needed
+		// Not needed
 		break;
 	case PAUSED:
-      // Not needed
+		// Not needed
 		break;
 	case RUNNING:
-      // Not needed
+		// Not needed
 		break;
 	}
 }
@@ -101,17 +101,17 @@ void Game::ChangeState(GameState newState)
 ErrorType Game::Setup(bool bFullScreen, HWND hwnd, HINSTANCE hinstance)
 {
 	// Create the engines - this should be done before creating other DDraw objects
-	if(FAILED(MyDrawEngine::Start(hwnd, bFullScreen)))
+	if (FAILED(MyDrawEngine::Start(hwnd, bFullScreen)))
 	{
 		ErrorLogger::Writeln(L"Failed to start MyDrawEngine");
 		return FAILURE;
 	}
-	if(FAILED(MySoundEngine::Start(hwnd)))
+	if (FAILED(MySoundEngine::Start(hwnd)))
 	{
 		ErrorLogger::Writeln(L"Failed to start MySoundEngine");
 		return FAILURE;
 	}
-	if(FAILED(MyInputs::Start(hinstance, hwnd)))
+	if (FAILED(MyInputs::Start(hinstance, hwnd)))
 	{
 		ErrorLogger::Writeln(L"Failed to start MyInputs");
 		return FAILURE;
@@ -126,13 +126,13 @@ ErrorType Game::Setup(bool bFullScreen, HWND hwnd, HINSTANCE hinstance)
 void Game::Shutdown()
 
 {
-   // Any clean up code here 
+	// Any clean up code here 
 
 
 
 
 
-	// (engines must be terminated last)
+	 // (engines must be terminated last)
 	MyDrawEngine::Terminate();
 	MySoundEngine::Terminate();
 	MyInputs::Terminate();
@@ -151,53 +151,53 @@ ErrorType Game::PauseMenu()
 {
 	// Code for a basic pause menu
 
-	MyDrawEngine::GetInstance()->WriteText(450,220, L"Paused", MyDrawEngine::WHITE);
+	MyDrawEngine::GetInstance()->WriteText(450, 220, L"Paused", MyDrawEngine::WHITE);
 
 	const int NUMOPTIONS = 2;
-	wchar_t options[NUMOPTIONS][11] = {L"Resume", L"Main menu"};
+	wchar_t options[NUMOPTIONS][11] = { L"Resume", L"Main menu" };
 
-   // Display menu options
-	for(int i=0;i<NUMOPTIONS;i++)
+	// Display menu options
+	for (int i = 0; i < NUMOPTIONS; i++)
 	{
 		int colour = MyDrawEngine::GREY;       // If not selected, should be grey
-		if(i == m_menuOption)
+		if (i == m_menuOption)
 		{
 			colour = MyDrawEngine::WHITE;       // Current selection is white
 		}
-		MyDrawEngine::GetInstance()->WriteText(450,300+50*i, options[i], colour);
+		MyDrawEngine::GetInstance()->WriteText(450, 300 + 50 * i, options[i], colour);
 	}
 
 	MyInputs* pInputs = MyInputs::GetInstance();
 
-   // Get user input
+	// Get user input
 	pInputs->SampleKeyboard();
 
-   // Move choice up and down
-	if(pInputs->NewKeyPressed(DIK_UP))
+	// Move choice up and down
+	if (pInputs->NewKeyPressed(DIK_UP))
 	{
 		m_menuOption--;
 	}
-	if(pInputs->NewKeyPressed(DIK_DOWN))
+	if (pInputs->NewKeyPressed(DIK_DOWN))
 	{
 		m_menuOption++;
 	}
-	if(m_menuOption<0)
+	if (m_menuOption < 0)
 	{
-		m_menuOption=0;
+		m_menuOption = 0;
 	}
-	else if(m_menuOption>=NUMOPTIONS)
+	else if (m_menuOption >= NUMOPTIONS)
 	{
-		m_menuOption=NUMOPTIONS-1;
+		m_menuOption = NUMOPTIONS - 1;
 	}
 
-   // If player chooses an option ....
-	if(pInputs->NewKeyPressed(DIK_RETURN))
+	// If player chooses an option ....
+	if (pInputs->NewKeyPressed(DIK_RETURN))
 	{
-		if(m_menuOption ==0)      // Resume
+		if (m_menuOption == 0)      // Resume
 		{
 			ChangeState(RUNNING);  // Go back to running the game
 		}
-		if(m_menuOption ==1)      // Quit
+		if (m_menuOption == 1)      // Quit
 		{
 			EndOfGame();           // Clear up the game
 			ChangeState(MENU);     // Go back to the menu
@@ -212,61 +212,61 @@ ErrorType Game::PauseMenu()
 // which is currently a basic placeholder
 ErrorType Game::MainMenu()
 {
-	MyDrawEngine::GetInstance()->WriteText(450,220, L"Main menu", MyDrawEngine::WHITE);
+	MyDrawEngine::GetInstance()->WriteText(450, 220, L"Main menu", MyDrawEngine::WHITE);
 
 	const int NUMOPTIONS = 3;
-	wchar_t options[NUMOPTIONS][15] = {L"Start game", L"Go Fullscreen",L"Exit"};
+	wchar_t options[NUMOPTIONS][15] = { L"Start game", L"Go Fullscreen",L"Exit" };
 
-   // If the window is currently fullscreen, change the second option to read "Go Windowed" instead
-	if(MyDrawEngine::GetInstance()->IsWindowFullScreen())
+	// If the window is currently fullscreen, change the second option to read "Go Windowed" instead
+	if (MyDrawEngine::GetInstance()->IsWindowFullScreen())
 	{
-		wcscpy_s( options[1], 15, L"Go Windowed");
+		wcscpy_s(options[1], 15, L"Go Windowed");
 	}
 
-   // Display the options
-	for(int i=0;i<NUMOPTIONS;i++)
+	// Display the options
+	for (int i = 0; i < NUMOPTIONS; i++)
 	{
 		int colour = MyDrawEngine::GREY;
-		if(i == m_menuOption)
+		if (i == m_menuOption)
 		{
 			colour = MyDrawEngine::WHITE;
 		}
-		MyDrawEngine::GetInstance()->WriteText(450,300+50*i, options[i], colour);
+		MyDrawEngine::GetInstance()->WriteText(450, 300 + 50 * i, options[i], colour);
 	}
 
-   // Get keyboard input
+	// Get keyboard input
 	MyInputs* pInputs = MyInputs::GetInstance();
 
 	pInputs->SampleKeyboard();
-	if(pInputs->NewKeyPressed(DIK_UP))
+	if (pInputs->NewKeyPressed(DIK_UP))
 	{
 		m_menuOption--;
 	}
-	if(pInputs->NewKeyPressed(DIK_DOWN))
+	if (pInputs->NewKeyPressed(DIK_DOWN))
 	{
 		m_menuOption++;
 	}
-	if(m_menuOption<0)
+	if (m_menuOption < 0)
 	{
-		m_menuOption=0;
+		m_menuOption = 0;
 	}
-	else if(m_menuOption>=NUMOPTIONS)
+	else if (m_menuOption >= NUMOPTIONS)
 	{
-		m_menuOption=NUMOPTIONS-1;
+		m_menuOption = NUMOPTIONS - 1;
 	}
 
-   // User selects an option
-	if(pInputs->NewKeyPressed(DIK_RETURN))
+	// User selects an option
+	if (pInputs->NewKeyPressed(DIK_RETURN))
 	{
-		if(m_menuOption ==0)          // Play
-		{  
+		if (m_menuOption == 0)          // Play
+		{
 			StartOfGame();             // Initialise the game
 			ChangeState(RUNNING);      // Run it
 		}
 
-		if(m_menuOption ==1)          // Toggle full screen
+		if (m_menuOption == 1)          // Toggle full screen
 		{
-			if(MyDrawEngine::GetInstance()->IsWindowFullScreen())
+			if (MyDrawEngine::GetInstance()->IsWindowFullScreen())
 			{
 				MyDrawEngine::GetInstance()->GoWindowed();
 			}
@@ -275,7 +275,7 @@ ErrorType Game::MainMenu()
 				MyDrawEngine::GetInstance()->GoFullScreen();
 			}
 		}
-		if(m_menuOption ==2)          //Quit
+		if (m_menuOption == 2)          //Quit
 		{
 			ChangeState(GAMEOVER);
 		}
@@ -293,21 +293,12 @@ ErrorType Game::MainMenu()
 // Use this to initialise the core game
 ErrorType Game::StartOfGame()
 {
-   // Code to set up your game *********************************************
-   // **********************************************************************
-	
-	MyDrawEngine* pDE = MyDrawEngine::GetInstance();
-	image = pDE->LoadPicture(L"spaceship.bmp");
+	// Code to set up your game *********************************************
+	// **********************************************************************
 
-	MyInputs* pInputs = MyInputs::GetInstance();
-	pInputs->SampleKeyboard();
+	p = new Player();
+	p->PlayerStart();
 
-	shipPos = Vector2D(-200, 300); //initialise the ships starting position
-	moveShip = Vector2D(1.5f, 1.5f); // initialise the ships movement speed
-
-	   
-
-	
 
 	return SUCCESS;
 }
@@ -321,31 +312,28 @@ ErrorType Game::Update()
 {
 	// Check for entry to pause menu
 	static bool escapepressed = true;
-	if(KEYPRESSED(VK_ESCAPE))
+	if (KEYPRESSED(VK_ESCAPE))
 	{
-		if(!escapepressed)
+		if (!escapepressed)
 			ChangeState(PAUSED);
-		escapepressed=true;
+		escapepressed = true;
 	}
 	else
-		escapepressed=false;
+		escapepressed = false;
 
 
-   // Your code goes here *************************************************
-   // *********************************************************************
-	
-	MyDrawEngine* pDE = MyDrawEngine::GetInstance(); //gets the pointer to the draw engine
-	pDE->DrawAt(shipPos, image, 2.0f, 0.5f, 0.8f); //draws the image of the spaceship
+	// Your code goes here *************************************************
+	// *********************************************************************
 
-	shipPos = shipPos + moveShip; // updates the ship position
-
-	
-
-	
+	p->PlayerMovement();
+	p->PlayerUpdate();
 
 
-   // *********************************************************************
-   // *********************************************************************
+
+
+
+	// *********************************************************************
+	// *********************************************************************
 
 	return SUCCESS;
 }
@@ -357,13 +345,14 @@ ErrorType Game::Update()
 ErrorType Game::EndOfGame()
 // called when the game ends by returning to main menu
 {
-   // Add code here to tidy up ********************************************
-   // *********************************************************************
+	// Add code here to tidy up ********************************************
+	// *********************************************************************
+
+	delete p;
+	p = nullptr;
 
 
 
-	
 
 	return SUCCESS;
 }
-
--- Assignment2019/gamecode.h
+++ Assignment2019/gamecode.h
@@ -10,6 +10,7 @@
 #include "windows.h"
 #include "mydrawengine.h"
 #include "gametimer.h"
+#include "Player.h"
 
 // This is a hack for reading keyboard in situations where you don't want to
 // use a full input system.
@@ -25,13 +26,13 @@
 class Game
 {
 private:
-	enum GameState{MENU, PAUSED, RUNNING, GAMEOVER};
+	enum GameState { MENU, PAUSED, RUNNING, GAMEOVER };
 	GameState m_currentState;      // Current state of the game 
-                                  // Menu = start menu
-                                  // Paused = paused
-                                  // Running = the main game loop
-                                  // GAMEOVER = setting this state causes the program to close
-                                  //            after tidying up
+								  // Menu = start menu
+								  // Paused = paused
+								  // Running = the main game loop
+								  // GAMEOVER = setting this state causes the program to close
+								  //            after tidying up
 	void ChangeState(GameState newState);  // Use to change the state of the game to one of the states above
 	int m_menuOption;              // Tracks the currently selected menu option, during main or pause menu
 	Game();                        // Constructor
@@ -40,59 +41,57 @@ private:
 
 	GameTimer gt;
 
-	PictureIndex image;
-	Vector2D shipPos;
-	Vector2D moveShip;
+	Player* p;
 
-	static const int NUMDOTS = 2000;
+
+
+
+	/* static const int NUMDOTS = 2000;
 	Vector2D dots[NUMDOTS];
 	Vector2D dotVelocities[NUMDOTS];
+	*/
 
-	
+
 
 public:
 	static Game instance;      // Singleton instance
 
-	
+
 
    // Starts the game engines - Draw Engine, Sound Engine, Input Engine - singletons
    // This is called soon after the program runs
 	ErrorType Setup(bool bFullScreen, HWND hwnd, HINSTANCE hinstance);
 
-   // Terminates the game engines - Draw Engine, Sound Engine, Input Engine
-   // This is called just before the program exits
+	// Terminates the game engines - Draw Engine, Sound Engine, Input Engine
+	// This is called just before the program exits
 	void Shutdown();
 
-   // This is repeated, called every frame.
-   // It will run either Update( ), MainMenu() or PauseMenu() depending on the
-   // game state
+	// This is repeated, called every frame.
+	// It will run either Update( ), MainMenu() or PauseMenu() depending on the
+	// game state
 	ErrorType Main();
 
-   // Called each frame when in the pause state. Manages the pause menu
-   // which is currently a basic placeholder
+	// Called each frame when in the pause state. Manages the pause menu
+	// which is currently a basic placeholder
 	ErrorType PauseMenu();
 
-   // Called each frame when in the menu state. Manages the main menu,
-   // which is currently a basic placeholder.
+	// Called each frame when in the menu state. Manages the main menu,
+	// which is currently a basic placeholder.
 	ErrorType MainMenu();
 
-   // Called at the start of the game - when changing state from MENU to RUNNING
-   // Use this to initialise the core game
+	// Called at the start of the game - when changing state from MENU to RUNNING
+	// Use this to initialise the core game
 	ErrorType StartOfGame();
 
-   // Called each frame when in the RUNNING state.
-   // Checks for user pressing escape (which puts the game in the PAUSED state)
-   // Flips and clears the back buffer
-   // Gameplay programmer will develop this to create an actual game
+	// Called each frame when in the RUNNING state.
+	// Checks for user pressing escape (which puts the game in the PAUSED state)
+	// Flips and clears the back buffer
+	// Gameplay programmer will develop this to create an actual game
 	ErrorType Update();
 
-   // Called when the player ends the game
-   // Currently this is done from the PAUSED state, when returning to the main menu
-   // but could be done by the gameplay programmer in other situations
-   // This will be used by the gameplay programmer to clean up
+	// Called when the player ends the game
+	// Currently this is done from the PAUSED state, when returning to the main menu
+	// but could be done by the gameplay programmer in other situations
+	// This will be used by the gameplay programmer to clean up
 	ErrorType EndOfGame();
 };
-
-
-
-
